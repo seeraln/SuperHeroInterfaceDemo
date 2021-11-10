5219787d96b0732b526395413b171734b42c3006
Seeralan Saththiyaseelan <24449@student.dorset-college.ie>
2021-11-10 17:08:08 +0000

    Ajoutez des fichiers projet.

---
+++ SuperHeroInterfaceDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SuperHeroInterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperMan sm = new SuperMan();
+
+            sm.Fly();
+            sm.LookThroughObjects();
+
+            Batman bm = new Batman();
+
+            bm.Fly();
+            bm.LookThroughObjects();
+        }
+    }
+}
---
+++ SuperHeroInterfaceDemo/SuperHeroLibrary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperHeroInterfaceDemo
+{
+    public abstract class SuperHero
+    {
+       
+    }
+    public abstract class Alien : SuperHero
+    {
+
+    }
+    public abstract class Human : SuperHero
+    {
+
+    }
+    interface IFlyable
+    {
+        void Fly();
+    }
+    interface ISeeThroughThings
+    {
+        void LookThroughObjects();
+    }
+    public class Batman : Human, IFlyable, ISeeThroughThings
+    {
+        public void Fly()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("I defy gravity with my special powers");
+        }
+        public void LookThroughObjects()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("I have X Ray vision");
+        }
+    }
+    public class SuperMan : Alien, IFlyable , ISeeThroughThings
+    {
+        public void Fly()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("I defy gravity with my special powers");
+        }
+        public void LookThroughObjects()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("I have X Ray vision");
+        }
+    }
+}
